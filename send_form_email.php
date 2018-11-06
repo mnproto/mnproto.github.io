954be0e042a027aab5fba7118625aba74c67038e
mnproto <43732336+mnproto@users.noreply.github.com>
2018-11-06 17:57:42 -0600

    Update send_form_email.php

--- send_form_email.php
+++ send_form_email.php
@@ -67,14 +67,6 @@ if(isset($_POST['submit'])) {
 $headers = 'From: '.$email_from."\r\n".
 'Reply-To: '.$email_from."\r\n" .
 'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);  
-?>
- 
-<!-- include your own success html here -->
- 
-Thank you for contacting us. We will be in touch with you very soon.
- 
-<?php
- 
-}
+mail($email_to, $email_subject, $email_message, $headers);
+header("Location: index.html?mailsent");
 ?>
